642b3fc8c51620e78fd4fe2647e4e4d6e5cda302
meng_zhang <meng_zhang@allyes.com>
2013-11-25 15:26:20 +0800

    add install cache files

---
+++ php/config.php
@@ -0,0 +1,14 @@
+<?php 
+if (!defined('IN_CONTEXT')) die('access violation error!');
+class Config {
+public static $mysql_ext = 'mysql';
+public static $db_host = '127.0.0.1';
+public static $db_user = 'brandmonitor';
+public static $db_pass = 'u8j5f2h4';
+public static $db_name = 'stny';
+public static $port = '3306';
+public static $mysqli_charset = 'utf8';
+public static $tbl_prefix = 'ss_';
+public static $cookie_prefix = 'TzyABs_';
+public static $enable_db_debug = false;
+}?>
